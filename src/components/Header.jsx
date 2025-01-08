65a963ab1be5480243a5100520f76d1e78997d8e
abolraj <abolfazlrajaeenasab@gmail.com>
2025-01-09 02:35:01 +0330

    feat - Handle Header and contents

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
-export default function Header({...props}){
+import Navigation from "./Navigation";
+
+export default function Header({ ...props }) {
     return (
-        <>
-        </>
+        <header class="bg-black fixed top-0 left-0 w-full h-screen z-50 p-4 wow fadeInUp" id="mh-header">
+            <Navigation></Navigation>
+        </header>
     )
 }
