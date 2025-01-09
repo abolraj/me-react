029e3623a55fe806e21e5b1272574110a04457a3
abolraj <abolfazlrajaeenasab@gmail.com>
2025-01-09 15:22:08 +0330

    feat - Add banner logo

--- src/parts/Header.jsx
+++ src/parts/Header.jsx
@@ -1,8 +1,12 @@
 import Navigation from "../components/Navigation";
+import bannerImg from "@assets/abolfazl.png"
 
 export default function Header({ ...props }) {
     return (
-        <header class="bg-black fixed top-0 left-0 w-full h-screen z-50 p-4 wow fadeInUp" id="mh-header">
+        <header class="w-full z-50" id="mh-header">
+            <section className="brand text-center">
+                <img src={bannerImg} alt="abolfazl" className="mx-auto"/>
+            </section>
             <Navigation></Navigation>
         </header>
     )
