4261f56f14eace10641c69bbe825187a268e7069
abolraj <abolfazlrajaeenasab@gmail.com>
2025-01-09 18:40:26 +0330

    fix - Fix the sticky position

--- src/parts/Header.jsx
+++ src/parts/Header.jsx
@@ -3,9 +3,9 @@ import bannerImg from "@assets/abolfazl.png"
 
 export default function Header({ ...props }) {
     return (
-        <header class="w-full z-50" id="mh-header">
-            <section className="brand text-center">
-                <img src={bannerImg} alt="abolfazl" className="mx-auto"/>
+        <header class="w-full z-50 sticky -top-28" id="mh-header">
+            <section className="brand text-center h-28">
+                <img src={bannerImg} alt="abolfazl" className="mx-auto h-full"/>
             </section>
             <Navigation></Navigation>
         </header>
