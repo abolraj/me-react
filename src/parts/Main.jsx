44d4b658edc3b9324a2abaf1c5fc922e9ceac303
abolraj <abolfazlrajaeenasab@gmail.com>
2025-01-09 15:21:46 +0330

    feat - Add and handle Main part

--- src/parts/Main.jsx
+++ src/parts/Main.jsx
@@ -1,6 +1,9 @@
+import Hero from "../components/Hero";
+
 export default function Main({...props}){
     return (
-        <>
-        </>
+        <main className="text-base-300">
+            <Hero></Hero>
+        </main>
     )
 }
