832310d838009b2d36e477b06f7aacc7f872cbae
abolraj <abolfazlrajaeenasab@gmail.com>
2025-01-09 18:55:22 +0330

    fix - Fix animation overflow

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -6,26 +6,26 @@ export default function Information({ ...props }) {
     return (
         <section class="about mt-24" id="about">
             <div class="container mx-auto">
-                <div class="row flex flex-wrap">
+                <div class="row flex flex-wrap overflow-hidden">
                     <div class="w-full md:w-1/2 px-4">
-                        <div class="about-img shadow-lg w-96" data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">
+                        <div class="about-img shadow-lg w-96" data-aos="fade-right" data-aos-duration="800" data-aos-delay="400">
                             <img src={devImg} alt="Developer Image" class="img-fluid h-full" />
                         </div>
                     </div>
                     <div class="w-full md:w-1/2 px-4">
                         <div class="about-inner text-base-content text-left">
-                            <h2 class="text-4xl font-bold mb-4 " data-aos="fade-up" data-aos-duration="800" data-aos-delay="100">
+                            <h2 class="text-4xl font-bold mb-4 " data-aos="fade-left" data-aos-duration="800" data-aos-delay="100">
                                 About Me &nbsp;
                                 <FontAwesomeIcon icon={faInfoCircle}/>
                             </h2>
-                            <p class="text-lg mb-6 aos-init aos-animate" data-aos="fade-up" data-aos-duration="800" data-aos-delay="200">
+                            <p class="text-lg mb-6 aos-init aos-animate" data-aos="fade-left" data-aos-duration="800" data-aos-delay="200">
                                 Hello, I’m <strong className="text-info">Abolfazl</strong>, a web developer based in Mashhad.
                                 I am a talented web developer specializing in <strong className="text-error">Laravel</strong>.
                                 With expertise in PHP, MySQL, HTML, CSS, and JS, my skillset is diverse and dynamic.
                                 Passionate about coding intelligently, I aim to create innovative and impactful solutions.
                                 With my in-depth understanding of web development, I consistently craft exceptional experiences for users.
                             </p>
-                            <div class="about-tag flex flex-wrap gap-2 mb-6 " data-aos="fade-up" data-aos-duration="800" data-aos-delay="300">
+                            <div class="about-tag flex flex-wrap gap-2 mb-6 " data-aos="fade-left" data-aos-duration="800" data-aos-delay="300">
                                 <span class="badge badge-primary badge-outline p-4">Laravel</span>
                                 <span class="badge badge-primary badge-outline p-4">PHP</span>
                                 <span class="badge badge-primary badge-outline p-4">MySQL</span>
@@ -36,8 +36,8 @@ export default function Information({ ...props }) {
                                 <span class="badge badge-primary badge-outline p-4">CSS</span>
                                 <span class="badge badge-primary badge-outline p-4">JavaScript</span>
                             </div>
-                            <a href="resume.pdf" class=" " >
-                                <button className="btn btn-primary">
+                            <a href="resume.pdf">
+                                <button className="btn btn-primary" data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">
                                     Download CV
                                     <FontAwesomeIcon icon={faDownload} />
                                 </button>
