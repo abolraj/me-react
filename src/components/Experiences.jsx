725bd0a101aa6942590cb720c554a31386e2a07f
abolraj <abolfazlrajaeenasab@gmail.com>
2025-01-14 22:17:07 +0330

    fix - Fix responsive size issues

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -46,32 +46,19 @@ const works = [
         ],
         delay: 600
     },
-    {
-        title: "Web Development At",
-        company: "Parscoders",
-        link: "https://parscoders.com/resume/306680/abol",
-        year: "2021-present",
-        responsibilities: [
-            "WordPress Development (themes, plugins, Elementor, WooCommerce)",
-            "Laravel Development (APIs, and more)",
-            "PHP Development",
-            "HTML, CSS, JavaScript"
-        ],
-        delay: 600
-    },
 ];
 
 export default function Experiences({ ...props }) {
     return (
         <section className="experience text-base-content" id="experience">
-            <div className="container mx-auto px-4">
+            <div className="container mx-auto sm:px-4">
                 <div className="flex flex-wrap my-8">
-                    <div className="w-full md:w-1/2 mb-8 p-2">
+                    <div className="w-full md:w-1/2 mb-8 sm:p-2">
                         <div className="education">
                             <h3 className="text-3xl font-extrabold text-gradient mb-6" data-aos="fade-up" data-aos-duration="800" data-aos-delay="200">🎓 Education</h3>
                             <div className="education-details">
                                 {educations.map((education, index) => (
-                                    <div key={index} className="education-item p-6 rounded-xl shadow-2xl mb-6" data-aos="fade-up" data-aos-duration="800" data-aos-delay={education.delay}>
+                                    <div key={index} className="education-item bg-base-100 p-6 rounded-xl shadow-2xl mb-6" data-aos="fade-up" data-aos-duration="800" data-aos-delay={education.delay}>
                                         <h4 className="text-2xl font-semibold mb-3">{education.title} <a href={education.link} className="text-accent hover:underline">{education.institution}</a></h4>
                                         <div className="text-sm text-info mb-2">{education.year}</div>
                                         <p className="text-left text-base-content">{education.description}</p>
@@ -80,16 +67,16 @@ export default function Experiences({ ...props }) {
                             </div>
                         </div>
                     </div>
-                    <div className="w-full md:w-1/2 mb-8 p-2">
+                    <div className="w-full md:w-1/2 mb-8 sm:p-2">
                         <div className="work">
                             <h3 className="text-3xl font-extrabold text-gradient mb-6">💼 Work Experience</h3>
                             <div className="experience-details">
                                 {works.map((work, index) => (
-                                    <div key={index} className="work-item p-6 rounded-xl shadow-2xl mb-6" data-aos="fade-up" data-aos-duration="800" data-aos-delay={work.delay}>
+                                    <div key={index} className="work-item bg-base-100 p-6 rounded-xl shadow-2xl mb-6" data-aos="fade-up" data-aos-duration="800" data-aos-delay={work.delay}>
                                         <h4 className="text-2xl font-semibold mb-3 text-center">{work.title} <a href={work.link} className="text-accent hover:underline">{work.company}</a></h4>
                                         <div className="text-sm text-info text-center mb-2">{work.year}</div>
                                         <span className="font-medium text-left w-full block mb-2">Responsibilities:</span>
-                                        <ul className="work-responsibility text-left list-['⭐'] list-inside ml-6">
+                                        <ul className="work-responsibility text-left list-['⭐'] list-inside ml-3">
                                             {work.responsibilities.map((responsibility, idx) => (
                                                 <li key={idx} className="my-2">{responsibility}</li>
                                             ))}
