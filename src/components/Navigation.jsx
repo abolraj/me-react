ac1086aa57b29db66cbdc5c63d781259cc1d962b
abolraj <abolfazlrajaeenasab@gmail.com>
2025-01-16 13:19:02 +0330

    fix - Add icons to mobile mode and redesign data-drivnly

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,44 +1,35 @@
-import { faBars, faHamburger } from "@fortawesome/free-solid-svg-icons";
+import { faAt, faAward, faBars, faBriefcase, faCodeBranch, faDollarSign, faDumbbell, faInfo, faMugHot, faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const navItems = [
+    { icon: faMugHot, label: 'Home', href: '#home' },
+    { icon: faInfo, label: 'About', href: '#about' },
+    { icon: faDumbbell, label: 'Skills', href: '#skills' },
+    { icon: faBriefcase, label: 'Experiences', href: '#experience' },
+    { icon: faAward, label: 'Certifications', href: '#certifications' },
+    { icon: faCodeBranch, label: 'Portfolio', href: '#portfolio' },
+    { icon: faDollarSign, label: 'Pricing', href: '#pricing' },
+    { icon: faStar, label: 'Rates', href: '#reviews' },
+    { icon: faAt, label: 'Contact', href: '#contact' },
+];
+
 export default function Navigation({ ...props }) {
     return (
-        <>
-            <div class="">
-                <nav class="dropdown max-lg:fixed max-lg:bg-transparent max-lg:left-2 z-50 max-lg:top-2 max-lg:aspect-square max-lg:float-left  bg-base-100 rounded-box">
-                    <button tabIndex="0" role="button" className="btn btn-primary bg-opacity-30 btn-circle lg:hidden backdrop-blur-sm shadow-xl">
-                        <FontAwesomeIcon icon={faBars} className="h-1/2"/>
-                    </button>
-                    <ul tabIndex="0" class="menu max-lg:dropdown-content lg:w-full flex-nowrap overflow-x-auto max-lg:mt-2  max-h-[60vh] lg:menu-horizontal  max-lg:flex-col bg-base-100 flex justify-around gap-2 p-2 rounded-box">
-                        <li class="active">
-                            <a class="text-base-content" href="#home">Home</a>
+        <div>
+            <nav class="dropdown max-lg:fixed max-lg:bg-transparent max-lg:left-2 z-50 max-lg:top-2 max-lg:aspect-square max-lg:float-left  bg-base-100 rounded-box">
+                <button tabIndex="0" role="button" className="btn btn-primary bg-opacity-30 btn-circle lg:hidden backdrop-blur-sm shadow-xl">
+                    <FontAwesomeIcon icon={faBars} className="h-1/2" />
+                </button>
+                <ul tabIndex="0" class="menu max-lg:dropdown-content lg:w-full flex-nowrap overflow-x-auto max-lg:mt-2  max-h-[60vh] lg:menu-horizontal  max-lg:flex-col bg-base-100 flex justify-around gap-2 p-2 rounded-box">
+                    {navItems.map((item, index) => (
+                        <li key={index} className={item.label === 'Home' ? 'active' : ''}>
+                            <a class="text-base-content pl-2" href={item.href}>
+                                <FontAwesomeIcon icon={item.icon} className="lg:hidden w-5 text-primary" />
+                                {item.label}
+                            </a>
                         </li>
-                        <li>
-                            <a class="text-base-content" href="#about">About</a>
-                        </li>
-                        <li>
-                            <a class="text-base-content" href="#skills">Skills</a>
-                        </li>
-                        <li>
-                            <a class="text-base-content" href="#experience">Experiences</a>
-                        </li>
-                        <li>
-                            <a class="text-base-content" href="#certifications">Certifications</a>
-                        </li>
-                        <li>
-                            <a class="text-base-content" href="#portfolio">Portfolio</a>
-                        </li>
-                        <li>
-                            <a class="text-base-content" href="#pricing">Pricing</a>
-                        </li>
-                        <li>
-                            <a class="text-base-content" href="#reviews">Rates</a>
-                        </li>
-                        <li>
-                            <a class="text-base-content" href="#contact">Contact</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </>
+                    ))}
+                </ul>
+            </nav>
+        </div>
     )
 }
