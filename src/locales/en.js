c7bbea367dc198182015598e0c5270699382de65
abolraj <abolfazlrajaeenasab@gmail.com>
2025-01-18 00:09:25 +0330

    feat - Add english localization

---
+++ src/locales/en.js
@@ -0,0 +1,376 @@
+export default {
+    "certifications": {
+        "title": "Certifications",
+        "categories": {
+            "all_categories": "All Categories",
+            "backend": "Backend",
+            "frontend": "Frontend"
+        },
+        "certificate": {
+            "laravel": {
+                "title": "Laravel",
+                "subTitle": "Programming Hub"
+            },
+            "rdbms": {
+                "title": "RDBMS",
+                "subTitle": "Programming Hub"
+            },
+            "redis": {
+                "title": "Redis",
+                "subTitle": "Programming Hub"
+            },
+            "seo": {
+                "title": "SEO",
+                "subTitle": "Programming Hub"
+            },
+            "react": {
+                "title": "ReactJs",
+                "subTitle": "Programming Hub"
+            },
+            "responsive_design": {
+                "title": "Responsive Web Design",
+                "subTitle": "Free Code Camp"
+            },
+            "typescript": {
+                "title": "Typescript",
+                "subTitle": "Programming Hub"
+            }
+        },
+        "modal": {
+            "close_button": "✕",
+            "check_it_button": "Check it!"
+        }
+    },
+    "contact": {
+        "title": "Contact Me",
+        "form": {
+            "name": {
+                "label": "Name",
+                "placeholder": "Name"
+            },
+            "email": {
+                "label": "Email",
+                "placeholder": "Email"
+            },
+            "phone": {
+                "label": "Phone No.",
+                "placeholder": "Phone No."
+            },
+            "message": {
+                "label": "Message",
+                "placeholder": "Message"
+            },
+            "send_button": "Send Message"
+        },
+        "alerts": {
+            "success": "Message sent successfully!",
+            "error": "Failed to send message. Please try again."
+        }
+    },
+    "experiences": {
+        "education": {
+            "title": "🎓 Education",
+            "items": [
+                {
+                    "title": "Web Development From",
+                    "institution": "Programming Hub",
+                    "year": "2022-2023",
+                    "description": "An immersive experience that honed my skills in Web Development, including Laravel, RDBMS, PHP, React, and more."
+                },
+                {
+                    "title": "Web Development From",
+                    "institution": "Free Code Camp",
+                    "year": "2022-2023",
+                    "description": "A transformative journey through Responsive Web Design and other essential web technologies."
+                }
+            ]
+        },
+        "work": {
+            "title": "💼 Work Experience",
+            "items": [
+                {
+                    "title": "Web Development At",
+                    "company": "Ponisha",
+                    "year": "2021-present",
+                    "responsibilities": [
+                        "WordPress Development (themes, plugins, Elementor, WooCommerce)",
+                        "Laravel Development (APIs, and more)",
+                        "PHP Development",
+                        "HTML, CSS, JavaScript"
+                    ]
+                },
+                {
+                    "title": "Web Development At",
+                    "company": "Parscoders",
+                    "year": "2021-present",
+                    "responsibilities": [
+                        "WordPress Development (themes, plugins, Elementor, WooCommerce)",
+                        "Laravel Development (APIs, and more)",
+                        "PHP Development",
+                        "HTML, CSS, JavaScript"
+                    ]
+                }
+            ]
+        }
+    },
+    "hero": {
+        "greeting": "Hello I'm",
+        "first-name": "Abolfazl",
+        "last-name": "Rajaee nasab",
+        "role": "Fullstack Web Developer [High-Backend]",
+        "contact": {
+            "email": "fazlabol18@gmail.com",
+            "phone": "+98 904 542 6387",
+            "location": "Mashhad, Razavi Khorasan, Iran",
+        },
+        "social": {
+            "github": {
+                "label": "GitHub",
+                "icon": "faGithub"
+            },
+            "linkedin": {
+                "label": "LinkedIn",
+                "icon": "faLinkedin"
+            },
+            "telegram": {
+                "label": "Telegram",
+                "icon": "faTelegram"
+            }
+        }
+    },
+    "information": {
+        "title": "About Me",
+        "description": {
+            "part1": "Hello, I’m",
+            "name": "Abolfazl",
+            "part2": ", a web developer based in Mashhad. I am a talented web developer specializing in",
+            "specialization": "Laravel",
+            "part3": ". With expertise in PHP, MySQL, HTML, CSS, and JS, my skillset is diverse and dynamic. Passionate about coding intelligently, I aim to create innovative and impactful solutions. With my in-depth understanding of web development, I consistently craft exceptional experiences for users."
+        },
+        "skills": [
+            "Laravel",
+            "PHP",
+            "MySQL",
+            "WordPress",
+            "Bootstrap 5",
+            "Tailwind",
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+        "download_button": "Download CV"
+    },
+    "navigation": {
+        "items": [
+            { "label": "Home", "href": "#home" },
+            { "label": "About", "href": "#about" },
+            { "label": "Skills", "href": "#skills" },
+            { "label": "Experiences", "href": "#experience" },
+            { "label": "Certifications", "href": "#certifications" },
+            { "label": "Portfolio", "href": "#portfolio" },
+            { "label": "Pricing", "href": "#pricing" },
+            { "label": "Rates", "href": "#reviews" },
+            { "label": "Contact", "href": "#contact" }
+        ]
+    },
+    "portfolios": {
+        "title": "Recent Portfolio",
+        "filters": {
+            "all_categories": "All Categories",
+            "no_filter": "No Filter",
+
+        },
+        "items": {
+            "medical_products": {
+                "title": "Medical Products",
+                "subTitle": "Fullstack - Laravel",
+                "tags": ["Laravel", "MySQL", "TailwindCSS"]
+            },
+            "medical_products_2": {
+                "title": "Medical Products",
+                "subTitle": "Fullstack - Laravel",
+                "tags": ["Laravel", "MySQL", "TailwindCSS"]
+            },
+            "recipe_finder_1": {
+                "title": "Recipe Finder",
+                "subTitle": "React + Tailwind + jQuery",
+                "tags": ["ReactJs", "TailwindCSS"]
+            },
+            "recipe_finder_2": {
+                "title": "Recipe Finder",
+                "subTitle": "React + Tailwind + jQuery",
+                "tags": ["ReactJs", "TailwindCSS"]
+            },
+            "todo_1": {
+                "title": "Todo",
+                "subTitle": "React",
+                "tags": ["ReactJs"]
+            },
+            "todo_2": {
+                "title": "Todo",
+                "subTitle": "React",
+                "tags": ["ReactJs"]
+            },
+            "todo_3": {
+                "title": "Todo",
+                "subTitle": "React",
+                "tags": ["ReactJs"]
+            },
+            "login_form": {
+                "title": "Login Form",
+                "subTitle": "Bootstrap 5",
+                "tags": ["Bootstrap5"]
+            },
+            "guess_word": {
+                "title": "Guess Word",
+                "subTitle": "HTML + CSS + Js",
+                "tags": []
+            }
+        },
+        "modal": {
+            "live_demo_button": "Live Demo",
+            "github_repo_button": "Github Repo",
+        },
+        "modals": {
+            "medical_products": {
+                "title": "Medical Products TSH - PHP 💊🛒",
+                "description": "This project is a mini framework developed by me! Includes models, DBMS, cache, and any other needed to develop this project.",
+                "tags": ["Laravel", "PHP", "MySQL", "Redis", "Memcached", "HTML", "CSS", "TailwindCSS", "Js", "jQuery"]
+            },
+            "recipe_finder": {
+                "title": "Recipe Finder",
+                "description": "This project helps you to find your own delicious foods everywhere with every ingredient you want! You can get started cooking by this helper, this guides you how to cook from zero; you can see the ingredients needed before any action!",
+                "tags": ["React", "TailwindCSS", "jQuery", "Vite", "HTML", "CSS", "Js"]
+            },
+            "todo": {
+                "title": "Todo - React",
+                "description": "This project is for managing your tasks.",
+                "tags": ["React", "Webpack", "HTML", "CSS", "Sass", "Js"]
+            },
+            "guess_word": {
+                "title": "Guess Word - My New Web Game [html+css+js] 😎",
+                "description": "You can guess the word, win if correct, lose if not correct. I tried to have beautiful UI & good UX & good handle back-end 🥲",
+                "tags": ["HTML", "CSS", "Js"]
+            },
+            "login_form": {
+                "title": "Login form - Bootstrap 5",
+                "description": "#Easy To Use My new experience about bootstrap is nice because of its easy use and style for the elements rather than CSS native. #Deeply Styling The bootstrap cannot handle deep styles, on the other hand we sometimes style natively! #Responsive Design This package makes nice easy responsive pages for you! E.g., using breakpoints sm, md, lg, xl, xxl",
+                "tags": ["HTML", "CSS", "Bootstrap5", "Sass", "UX", "UI"]
+            }
+        }
+    },
+    "pricing": {
+        "title": "Pricing Table",
+        "starting_from": "Starting from",
+        "hire_me": "Hire Me",
+        "plans": {
+            "full_time": {
+                "title": "Full-time work",
+                "description": "Ready to commit to your company full-time",
+                "price": "$1200 monthly",
+                "features": ["Web Development"]
+            },
+            "fixed_price": {
+                "title": "Fixed Price Project",
+                "description": "Available for specific project roles",
+                "price": "$12 hourly",
+                "features": ["Web Development"]
+            },
+            "hourly": {
+                "title": "Hourly work",
+                "description": "Open for hourly projects",
+                "price": "$12 hourly",
+                "features": ["Web Development"]
+            }
+        }
+    },
+    "reviews": {
+        "title": "Client Reviews",
+        "items": [
+            {
+                "rating": 5,
+                "text": "It was a very good collaboration. Precision in the work process, punctuality in delivery, and support after the completion of the work are three important characteristics that make me hopeful for future collaborations with him.",
+                "name": "Behzad Alavi",
+                "title": "Art College Stu. at Austria"
+            },
+            {
+                "rating": 5,
+                "text": "Completely professional and fast performance. I will definitely consult him for future projects.",
+                "name": "Exoplanetor",
+                "title": "Ponisha"
+            },
+            {
+                "rating": 5,
+                "text": "This is the third project we have done with him. He knows his work very well and has implemented all the project requirements. I wish him increasing success every day.",
+                "name": "Hatam",
+                "title": "Martial Art Researcher"
+            },
+            {
+                "rating": 5,
+                "text": "They delivered very quickly. The code was excellent and well-written. High proficiency in their work.",
+                "name": "Perkas",
+                "title": "Autoplay Carousel"
+            },
+            {
+                "rating": 5,
+                "text": "This is the second project I've done with him, and I am completely satisfied with the collaboration.",
+                "name": "Hatam",
+                "title": "Martial Art Researcher"
+            },
+            {
+                "rating": 5,
+                "text": "I would like to thank the team for their efforts on the project. They showed utmost dedication and collaboration. I will definitely work with this excellent team again.",
+                "name": "Hatam",
+                "title": "Martial Art Researcher"
+            }
+        ]
+    },
+    "services": {
+        "title": "What I Do as a Fullstack Developer 🔥🔥🔥",
+        "items": {
+            "frontend": {
+                "title": "Frontend Web Development ⭐⭐⭐",
+                "description": "With extensive hands-on experience, I design and develop responsive, interactive front-end interfaces using ReactJS, TailwindCSS, and JavaScript. Let's transform your vision into a stunning reality."
+            },
+            "backend": {
+                "title": "Backend Web Development ⭐⭐⭐⭐⭐",
+                "description": "Specializing in backend development with PHP, Laravel, and WordPress, I build robust server-side applications. From customizing plugins and themes to developing APIs, I ensure your server-side applications are efficient and scalable."
+            },
+            "support": {
+                "title": "Support ⭐⭐⭐⭐⭐",
+                "description": "I provide one month of free support for any project developed by me, ensuring smooth integration and optimal performance. Your success is my commitment."
+            }
+        }
+    },
+    "skills": {
+        "title": "Skills",
+        "subtitle": "A comprehensive list of my technical and professional skills",
+        "technical_skills_title": "Technical Skills",
+        "professional_skills_title": "Professional Skills",
+        "technical_skills": [
+            { "name": "Laravel ❤️", "percentage": 86 },
+            { "name": "PHP 🔥", "percentage": 90 },
+            { "name": "MySQL 🔥", "percentage": 90 },
+            { "name": "REST API", "percentage": 89 },
+            { "name": "WordPress", "percentage": 80 },
+            { "name": "Bootstrap 5", "percentage": 65 },
+            { "name": "React", "percentage": 56 },
+            { "name": "Tailwind", "percentage": 50 },
+            { "name": "HTML", "percentage": 85 },
+            { "name": "CSS", "percentage": 90 },
+            { "name": "JavaScript", "percentage": 88 }
+        ],
+        "professional_skills": [
+            { "name": "Communication", "percentage": 95 },
+            { "name": "Team Work", "percentage": 80 },
+            { "name": "Project Management", "percentage": 86 },
+            { "name": "Creativity", "percentage": 70 },
+            { "name": "Hard Work", "percentage": 90 },
+            { "name": "Flexible", "percentage": 80 },
+            { "name": "On Time", "percentage": 90 }
+        ]
+    },
+    "footer": {
+        "text": "2025 ©️ Developed and designed with ❤️ by me"
+    }
+}
