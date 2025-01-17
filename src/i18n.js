b92fcdb7d9425fe8f9ae1a85bf7da2d71027a6df
abolraj <abolfazlrajaeenasab@gmail.com>
2025-01-18 00:09:08 +0330

    feat - Add localization bootstrap file

---
+++ src/i18n.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import enResources from '@/locales/en.js'
+import faResources from '@/locales/fa.js'
+import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector/cjs";
+
+i18n
+    .use(I18nextBrowserLanguageDetector)
+    .use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+        // the translations
+        // (tip move them in a JSON file and import them,
+        // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+        resources: {
+            en: {
+                translation: enResources,
+            },
+            fa: {
+                translation: faResources,
+            },            
+        },
+        lng:"fa",
+        fallbackLng: "en",
+
+        interpolation: {
+            escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+        }
+    });
+
+export default i18n
