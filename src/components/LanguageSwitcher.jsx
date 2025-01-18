5150dc7464f45e6f84d73f20fad41b68fae5dc30
abolraj <abolfazlrajaeenasab@gmail.com>
2025-01-18 10:50:05 +0330

    feat - Add and handle LanguageSwitcher for change app locale

---
+++ src/components/LanguageSwitcher.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFlagUsa, faFlag, faFlagCheckered } from '@fortawesome/free-solid-svg-icons';
+import FlagIran from '@assets/svg/countries/iran.svg'
+import FlagPalestine from '@assets/svg/countries/palestine.svg'
+import FlagUK from '@assets/svg/countries/united_kingdom.svg'
+
+export default function LanguageSwitcher({ ...props }) {
+    const { i18n } = useTranslation();
+
+    // Supported languages
+    const languages = [
+        { code: 'en', name: 'English', icon: FlagUK },
+        { code: 'fa', name: 'فارسی', icon: FlagIran },
+        { code: 'ar', name: 'العربية', icon: FlagPalestine },
+    ];
+    
+    const [lang, setLang] = useState(languages[0])
+
+    // Change language
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+        window.location.reload(false)
+    };
+
+    // Update HTML lang attribute for SEO
+    useEffect(() => {
+        document.documentElement.lang = i18n.language;
+        setLang(languages.find((lang) => lang.code === i18n.language))
+    }, [i18n.language]);
+
+    return (
+        <div className="fixed bottom-2 left-2">
+            <div className="dropdown dropdown-top dropdown-right">
+                <label tabIndex={0} className="btn btn-ghost rounded-btn">
+                    <img src={lang.icon} loading='lazy' alt={"Flag "+lang.code} className='w-5 h-5'/>
+                </label>
+                <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                    {languages.map((lang) => (
+                        <li key={lang.code}>
+                            <button
+                                onClick={() => changeLanguage(lang.code)}
+                                className={`flex items-center gap-2 ${i18n.language === lang.code ? 'active' : ''}`}
+                            >
+                                <img src={lang.icon} loading='lazy' alt={"Flag "+lang.code} className='w-4 h-4'/>
+                                {/* <FontAwesomeIcon icon={lang.icon} className="w-4 h-4" /> */}
+                                <span>{lang.name}</span>
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
