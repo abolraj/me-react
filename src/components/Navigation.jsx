7a2a9ac2bbd6af6b96fe5f73cfa3c0a9f8b8d125
abolraj <abolfazlrajaeenasab@gmail.com>
2025-01-18 11:26:44 +0330

    fix - Fix responsive for rtl layouts

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -19,14 +19,14 @@ export default function Navigation({ ...props }) {
 
     return (
         <div>
-            <nav className="dropdown max-lg:fixed max-lg:bg-transparent max-lg:left-2 z-50 max-lg:top-2 max-lg:aspect-square max-lg:float-left bg-base-100 rounded-box">
+            <nav className="dropdown [&:lang(fa)]:dropdown-end max-lg:fixed max-lg:bg-transparent max-lg:start-2 z-50 max-lg:top-2 max-lg:aspect-square max-lg:float-left bg-base-100 rounded-box">
                 <button tabIndex="0" role="button" className="btn btn-primary bg-opacity-30 btn-circle lg:hidden backdrop-blur-sm shadow-xl">
                     <FontAwesomeIcon icon={faBars} className="h-1/2" />
                 </button>
                 <ul tabIndex="0" className="menu max-lg:dropdown-content lg:w-full flex-nowrap overflow-x-auto max-lg:mt-2 max-h-[60vh] lg:menu-horizontal max-lg:flex-col bg-base-100 flex justify-around gap-2 p-2 rounded-box">
                     {navItems.map((item, index) => (
                         <li key={index} className={item.label === t("navigation.items.0.label") ? 'active' : ''}>
-                            <a className="text-base-content pl-2" href={item.href}>
+                            <a className="text-base-content ps-2" href={item.href}>
                                 <FontAwesomeIcon icon={item.icon} className="lg:hidden w-5 text-primary" />
                                 {item.label}
                             </a>
