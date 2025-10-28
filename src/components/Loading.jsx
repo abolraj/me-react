040a0d872c1d064c6c3f455fadc562b8ea1c422d
abolraj <abolfazlrajaeenasab@gmail.com>
2025-10-28 23:32:03 +0330

    feat(components): Add Loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+    return (
+        <div className="fixed size-full flex items-center justify-center">
+            <div className="loading loading-infinity">
+            </div>
+        </div>
+    )
+}
