ea4842d2e37f34b6570eca818844219b6f75f554
abolraj <abolfazlrajaeenasab@gmail.com>
2025-10-28 23:35:53 +0330

    feat(tailwind.config): Add daysiUI themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,10 @@ export default {
   plugins: [
     require('daisyui')
   ],
+
+  daisyui: {
+    themes: ['light', 'dark'], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "dark", // name of one of the included themes for dark mode
+  },
 }
 
