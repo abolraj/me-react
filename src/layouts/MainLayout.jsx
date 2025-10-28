91879935a8cf91e003f9c72aeb0816350c4e536e
abolraj <abolfazlrajaeenasab@gmail.com>
2025-10-28 23:34:06 +0330

    feat(layouts/MainLayout): Add tiny padding

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -6,7 +6,7 @@ export default function MainLayout({
 }) {
 
     return (
-        <div>
+        <div className="p-2">
             <Header />
             <main className="text-base-300">
                 {children}
