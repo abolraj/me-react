fe94bf18ed2633e2a85d5263dbc2f72d82a85015
abolraj <abolfazlrajaeenasab@gmail.com>
2025-10-29 00:19:23 +0330

    feat(pages): Add maintenance mode

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,9 +1,13 @@
+import MainLayout from "@/layouts/MainLayout";
+
 export default function ContactPage ({
 
 }) {
     return (
-        <div>
-
-        </div>
+        <MainLayout>
+            <p className="text-base-content text-8xl mt-10">
+            Under Deveopment ... ⚒️😅
+            </p>
+        </MainLayout>
     )
 }
--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,9 +1,13 @@
+import MainLayout from "@/layouts/MainLayout";
+
 export default function ServicesPage ({
 
 }) {
     return (
-        <div>
-
-        </div>
+        <MainLayout>
+            <p className="text-base-content text-8xl mt-10">
+            Under Deveopment ... ⚒️😅
+            </p>
+        </MainLayout>
     )
 }
