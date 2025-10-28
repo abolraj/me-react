0291e50f7527aeb28469bc5b36507fd853d194f1
abolraj <abolfazlrajaeenasab@gmail.com>
2025-10-28 18:08:23 +0330

    fix(App): Use as wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,26 @@
 import Aos from 'aos'
 import './App.css'
-import Footer from './parts/Footer'
-import Header from './parts/Header'
-import Main from './parts/Main'
 import { useEffect } from 'react'
 import 'aos/dist/aos.css'
 import './i18n.js'
 import { useTranslation } from 'react-i18next'
 
-function App() {
-  const {i18n} = useTranslation()
-  useEffect(()=>{
+function App({
+  children,
+}) {
+  const { i18n } = useTranslation()
+  useEffect(() => {
     document.documentElement.lang = i18n.language
     document.dir = i18n.dir()
     Aos.init({
       once: true
     })
-  },[i18n.language])
+  }, [i18n.language])
 
 
   return (
     <>
-      <Header></Header>
-      <Main></Main>
-      <Footer></Footer>
+      {children}
     </>
   )
 }
