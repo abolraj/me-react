6d3be116d4c26fea0ee1365d46e15cc1c229ea9a
abolraj <abolfazlrajaeenasab@gmail.com>
2025-10-31 15:43:20 +0330

    fix(components/LanguageSwitcher): Fix the style

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import FlagIran from '@assets/svg/countries/iran.svg'
 import FlagPalestine from '@assets/svg/countries/palestine.svg'
 import FlagUK from '@assets/svg/countries/united_kingdom.svg'
+import { Languages } from 'lucide-react';
 
 export default function LanguageSwitcher({ ...props }) {
     const { i18n } = useTranslation();
@@ -32,10 +33,13 @@ export default function LanguageSwitcher({ ...props }) {
     }, [i18n.language]);
 
     return (
-        <div className="h-full">
-            <div className="dropdown dropdown-end rtl:dropdown-end h-full min-h-min">
-                <label tabIndex={0} className="btn btn-ghost min-h-min size-10 sm:size-16 lg:size-16 md:p-4 p-1 sm:p-2 box-border rounded-full">
-                    <img src={lang.icon} loading='lazy' alt={"Flag "+lang.code} className='h-full'/>
+        <div className="max-lg:!fixed max-lg:left-2 max-lg:bottom-2">
+            <div className="dropdown dropdown-top lg:dropdown-bottom dropdown-start rtl:dropdown-end h-full min-h-min">
+                <label tabIndex={0} className={`relative btn btn-ghost min-h-min size-10 sm:size-16 lg:size-16 md:p-4 p-1 sm:p-2 box-border rounded-full`}>
+                    <Languages className='size-full text-base-content'/>
+                    <div className='absolute bottom-0 left-0 lg:left-3 lg:bottom-3 size-3'>
+                        <img src={lang.icon} loading='lazy' alt={"Flag "+lang.code} className='h-full'/>
+                    </div>
                 </label>
                 <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
                     {languages.map((lang) => (
