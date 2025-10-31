78d1ed73290b74867bd24820317b56eee0a9cb98
abolraj <abolfazlrajaeenasab@gmail.com>
2025-10-31 16:04:15 +0330

    fix(components/Navigation): Fix size

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -45,11 +45,11 @@ export default function Navigation({ ...props }) {
             <button tabIndex="0"
                 role="button"
                 id="header-menu-btn"
-                className="btn btn-ghost  z-50 relative focus:outline-none !border-none group-data-[showed-menu=1]:rotate-90 transition-transform min-h-min bg-opacity-30 btn-circle p-2 box-border lg:hidden size-10 sm:size-16 lg:size-20 text-base-content"
+                className="btn btn-ghost  z-50 relative focus:outline-none !border-none group-data-[showed-menu=1]:rotate-90 transition-transform min-h-min bg-opacity-30 btn-circle p-2 md:p-4 box-border lg:hidden size-10 sm:size-16 lg:size-20 text-base-content"
                 onClick={handleMenuClick}
             >
-                <Menu className="size-full p-2 transition-opacity left-0 top-0 absolute group-data-[showed-menu=1]:opacity-0" />
-                <X className="size-full max-sm:p-2 transition-opacit left-0 top-0 absolute opacity-0 group-data-[showed-menu=1]:opacity-100" />
+                <Menu className="size-full p-2 md:p-4 transition-opacity left-0 top-0 absolute group-data-[showed-menu=1]:opacity-0" />
+                <X className="size-full max-sm:p-2 md:p-4 transition-opacit left-0 top-0 absolute opacity-0 group-data-[showed-menu=1]:opacity-100" />
             </button>
             <ul tabIndex="0" className="max-lg:absolute max-lg:backdrop-blur-md max-lg:bg-base-300/40 max-lg:h-0 transition-[height,opacity] max-lg:group-data-[showed-menu=0]:opacity-0 max-lg:group-data-[showed-menu=1]:h-[max(60vh,20rem)] max-lg:top-16 z-0 left-0 max-lg:w-screen  flex-nowrap overflow-x-auto max-lg:flex-col max-lg:items-start flex justify-around gap-2 max-lg:p-3 overflow-hidden max-lg:overflow-auto rounded-b-box">
                 {navItems.map((item, index) => (
