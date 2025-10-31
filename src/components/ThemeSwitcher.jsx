5c41c6b2b9f6c8c3134158a934f4d1958b3c9470
abolraj <abolfazlrajaeenasab@gmail.com>
2025-10-31 16:04:33 +0330

    fix(components/ThemeSwitcher): Fix sizes

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -4,7 +4,7 @@ export default function ThemeSwitcher({ ...props }) {
     const { darkMode, setTheme } = useTheme();
 
     return (
-        <label className="btn btn-ghost h-full min-h-[inherit] size-10 sm:size-16 lg:size-16 p-1 sm:p-2 md:p-4 swap swap-rotate text-base-content">
+        <label className="btn btn-ghost btn-circle h-full min-h-[inherit] size-10 sm:size-16 lg:size-16 p-1 sm:p-2 md:p-4 swap swap-rotate text-base-content">
             <input type="checkbox" className="theme-controller" onChange={e=>setTheme(e.target.checked ? 'light' : 'dark')} value="light" checked={!darkMode} />
 
             <svg
