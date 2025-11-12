0ab6bddd1ce81559f3cc80a4b25585a9a5a7d4d9
abolraj <abolfazlrajaeenasab@gmail.com>
2025-11-12 14:03:48 +0330

    fix(layouts/MainLayout): Add overflow hidden

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -6,9 +6,9 @@ export default function MainLayout({
 }) {
 
     return (
-        <div className="p-2">
+        <div className="p-2 overflow-hidden">
             <Header />
-            <main className="text-base-300 box-border pt-12 sm:pt-20">
+            <main className="text-base-300 box-border pt-12 px-4 md:px-6 sm:pt-20">
                 {children}
             </main>
             <Footer />
