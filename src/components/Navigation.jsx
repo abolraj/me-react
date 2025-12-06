3269a10af81602a24bbb7ae5a6712591bf206283
abolraj <abolfazlrajaeenasab@gmail.com>
2025-12-07 00:49:32 +0330

    feat(components/Navigation): Handle tanstack router navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -10,6 +10,7 @@ import { faMugHot } from "@fortawesome/free-solid-svg-icons/faMugHot";
 import { faStar } from "@fortawesome/free-solid-svg-icons/faStar";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "@tanstack/react-router";
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
 import { useTranslation } from 'react-i18next'; // Import useTranslation
@@ -19,15 +20,15 @@ export default function Navigation({ ...props }) {
     const [isShowedMenu, setIsShowedMenu] = useState(false);
 
     const navItems = [
-        { icon: faMugHot, label: t("navigation.items.0.label"), href: t("navigation.items.0.href") },
-        { icon: faInfo, label: t("navigation.items.1.label"), href: t("navigation.items.1.href") },
-        { icon: faDumbbell, label: t("navigation.items.2.label"), href: t("navigation.items.2.href") },
-        { icon: faBriefcase, label: t("navigation.items.3.label"), href: t("navigation.items.3.href") },
-        { icon: faAward, label: t("navigation.items.4.label"), href: t("navigation.items.4.href") },
-        { icon: faCodeBranch, label: t("navigation.items.5.label"), href: t("navigation.items.5.href") },
-        { icon: faDollarSign, label: t("navigation.items.6.label"), href: t("navigation.items.6.href") },
-        { icon: faStar, label: t("navigation.items.7.label"), href: t("navigation.items.7.href") },
-        { icon: faAt, label: t("navigation.items.8.label"), href: t("navigation.items.8.href") },
+        { icon: faMugHot, label: t("navigation.home"), href: '/#home' },
+        { icon: faInfo, label: t("navigation.about"), href: '/#about' },
+        { icon: faDumbbell, label: t("navigation.skills"), href: '/#skills' },
+        { icon: faBriefcase, label: t("navigation.experiences"), href: '/#experience' },
+        { icon: faAward, label: t("navigation.certifications"), href: '/#certifications' },
+        { icon: faCodeBranch, label: t("navigation.portfolio"), href: '/#portfolio' },
+        { icon: faDollarSign, label: t("navigation.pricing"), href: '/#pricing' },
+        { icon: faStar, label: t("navigation.reviews"), href: '/#reviews' },
+        { icon: faAt, label: t("navigation.contact"), href: '/contact' },
     ];
 
     const handleMenuClick = () => {
@@ -54,11 +55,11 @@ export default function Navigation({ ...props }) {
             <ul tabIndex="0" className="max-lg:absolute max-lg:backdrop-blur-md max-lg:bg-base-300/40 max-lg:h-0 border-black max-lg:border-b max-lg:shadow-md transition-[height,opacity] max-lg:group-data-[showed-menu=0]:opacity-0 max-lg:group-data-[showed-menu=1]:h-[max(60vh,20rem)] max-lg:top-16 z-0 left-0 max-lg:w-screen  flex-nowrap overflow-x-auto max-lg:flex-col max-lg:items-start flex justify-around gap-2 max-lg:p-3 overflow-hidden max-lg:overflow-auto rounded-b-box">
                 {navItems.map((item, index) => (
                     <li key={index} className={`${item.label === t("navigation.items.0.label") ? 'active' : ''}`}>
-                        <a className="text-base-content max-lg:ps-2 text-2xl lg:text-lg  p-1" href={item.href}>
+                        <Link className="text-base-content max-lg:ps-2 text-2xl lg:text-lg  p-1" to={item.href}>
                             <FontAwesomeIcon icon={item.icon} className="lg:hidden w-5 text-primary" />
                             &nbsp;
                             {item.label}
-                        </a>
+                        </Link>
                     </li>
                 ))}
             </ul>
