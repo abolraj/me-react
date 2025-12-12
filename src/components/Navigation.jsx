b3c4f8195a378fd84da3591b262e72042efdcaa3
abolraj <abolfazlrajaeenasab@gmail.com>
2025-12-12 22:46:00 +0330

    fix(components/Navigation): Fix handling show

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -33,14 +33,19 @@ export default function Navigation({ ...props }) {
         { icon: faAt, label: t("navigation.contact"), href: '/contact' },
     ];
 
-    const handleMenuClick = () => {
+    const handleMenuClick = (isShowed = null) => {
         setIsShowedMenu(showed => {
-            showed = !showed;
+            showed = isShowed === null ? !showed : isShowed;
             document.body.style = showed ? "overflow:hidden;" : "";
 
             return showed;
         });
 
+    }
+
+    const handleLinkClick = (index) => {
+        setActiveNavIndex(index);
+        handleMenuClick(false);
     }
 
     return (
@@ -49,11 +54,14 @@ export default function Navigation({ ...props }) {
                 role="button"
                 id="header-menu-btn"
                 className="btn btn-ghost  z-50 relative focus:outline-none !border-none group-data-[showed-menu=1]:rotate-90 transition-transform min-h-min bg-opacity-30 btn-circle p-2 md:p-4 box-border lg:hidden size-10 sm:size-16 lg:size-20 text-base-content"
-                onClick={handleMenuClick}
+                onClick={() => handleMenuClick()}
             >
                 <Menu className="size-full p-2 md:p-4 transition-opacity left-0 top-0 absolute group-data-[showed-menu=1]:opacity-0" />
                 <X className="size-full max-sm:p-2 md:p-4 transition-opacit left-0 top-0 absolute opacity-0 group-data-[showed-menu=1]:opacity-100" />
             </button>
+            <div className="absolute top-0 left-0 w-screen h-screen group-data-[showed-menu=0]:hidden" onClick={() => handleMenuClick(false)}>
+
+            </div>
             <ul tabIndex="0" className="max-lg:absolute max-lg:backdrop-blur-md max-lg:bg-base-300/40 max-lg:h-0 border-black max-lg:border-b max-lg:shadow-md transition-[height,opacity] max-lg:group-data-[showed-menu=0]:opacity-0 max-lg:group-data-[showed-menu=1]:h-[max(60vh,20rem)] max-lg:top-16 z-0 left-0 max-lg:w-screen  flex-nowrap overflow-x-auto max-lg:flex-col max-lg:items-start flex justify-around gap-2 max-lg:p-3 overflow-hidden max-lg:overflow-auto rounded-b-box">
                 {navItems.map((item, index) => (
                     <li key={index} className={`${item.label === t("navigation.items.0.label") ? 'active' : ''}`}>
@@ -65,7 +73,7 @@ export default function Navigation({ ...props }) {
                                 includeHash: true,
                             }}
                             to={item.href}
-                            onClick={() => setActiveNavIndex(index)}
+                            onClick={() => handleLinkClick(index)}
                             data-active={+(index === activeNavIndex)}
                         >
                             <FontAwesomeIcon icon={item.icon} className="lg:hidden w-5 text-primary" />
