fb0c6be3c45534238243fd6321982c172bb5afe0
abolraj <abolfazlrajaeenasab@gmail.com>
2026-01-05 13:37:57 +0330

    fix(router/index): Add the main routes via second parent

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -7,27 +7,22 @@ import { CertificationsRoute } from './routes/CertificationsRoute'
 import { ServicesRoute } from './routes/ServicesRoute'
 import { ContactRoute } from './routes/ContactRoute'
 import { HomeRoute } from './routes/HomeRoute'
+import { MainLayoutRoute } from './routes/MainLayoutRoute'
 
 const routeTree = RootRoute.addChildren([
-    HomeRoute,
-    PortfoliosRoute,
-    ReviewsRoute,
-    CertificationsRoute,
-    ServicesRoute,
-    ContactRoute,
+    MainLayoutRoute.addChildren([
+        HomeRoute,
+        PortfoliosRoute,
+        ReviewsRoute,
+        CertificationsRoute,
+        ServicesRoute,
+        ContactRoute,
+    ])
 ])
 
-// We can add another new layout and its routes here
-// Every pages does not need to childed with Layout component 
-// cause of it handled in render component of the main Router (of that layout)
-// 
-// const dashboardRouteTree = DashboardRoute.addChildren([
-//     HomeRoute,
-//     AnaliticsRoute,
-//     ...
-// ])
 
 export const router = createRouter({
     routeTree,
+    notFoundMode: 'root',
     scrollRestoration: true,
 })
